84bfa855df8ac0d5cfc0bb4f1c0beed6b2d6c28c
Eff <peter.felber@turbine.ai>
2021-12-02 22:44:36 +0100

    2020 dec7/2 solved

--- 2020/dec7/2/main.py
+++ 2020/dec7/2/main.py
@@ -3,7 +3,7 @@ import pprint
 bagrules = {}
 
 next_set = set()
-with open('../input2.txt', 'r') as inputfile:
+with open('../input.txt', 'r') as inputfile:
     for line in inputfile:
         # remove newline
         line = line.strip()
@@ -32,74 +32,45 @@ with open('../input2.txt', 'r') as inputfile:
             # If a bag contains no aother bags, set the rule value to None
             bagrules[head] = None
 
+pp = pprint.PrettyPrinter(indent=2)
 # print parsed rules
 # pp.pprint(bagrules)
+# for key, value in bagrules.items():
+#     print(f"{key}: {value}")
 
 my_bags = {}
-# for key in bagrules:
-#     my_bags[key] = 0
-my_bags['shiny gold'] = 1
-
-pp = pprint.PrettyPrinter(indent=2)
-pp.pprint(my_bags)
 
 prev_bags = {
     'shiny gold': 1
 }
-new_bags = {}
+# this can be anything, just need to be not and empty list to start the cycle
+new_bags = 'start'
 
-for bag, quantity in prev_bags.items():
-    # if a bag containes other bags
-    if bagrules[bag] is not None:
-        # go through each rule for the bag
-        for new_bag_name, new_bag_value in bagrules[bag].items():
-            # add the new bags to new_bag multiplied by the quantity of bags we have
-            new_bags[new_bag_name] = new_bag_value*quantity
-# pp.pprint(new_bags)
+def get_contained_bags(current_bag_list):
+    new_bag_list = {}
+    for bag_type, quantity in current_bag_list.items():
+        if bagrules[bag_type] is not None:
+            for new_bag_type, new_bag_quanty in bagrules[bag_type].items():
+                if new_bag_type in new_bag_list.keys():
+                    new_bag_list[new_bag_type] += quantity*new_bag_quanty
+                else:
+                    new_bag_list[new_bag_type] = quantity*new_bag_quanty
+    return new_bag_list
 
-# add the new bags to the total number of bags
-for key, value in new_bags.items():
-    if key not in my_bags.keys():
-        my_bags[key] = value
-    else:
-        my_bags[key] += value
-
-pp.pprint(my_bags)
-cycle = 1
-# repeat cycle until we have no new bags to add
 while new_bags != {}:
-    # rotate variables
-    # in each cycle we want to add only the new bags contained inside the bags from in the previous cycle
-    prev_bags = new_bags.copy()
-    new_bags = {}
-
-    # go through each bag type from the previous cycle
-    for bag, quantity in prev_bags.items():
-        # if the bag contains other bags
-        if bagrules[bag] is not None:
-            # go through the rules for the current bag
-            for new_bag_name, new_bag_value in bagrules[bag].items():
-                # add the new bag to the variable, multiplied by the current quantity of containing bag
-                new_bags[new_bag_name] = new_bag_value*quantity
-    # pp.pprint(new_bags)
-    # add the new bags to the total number of bags
-    for key, value in new_bags.items():
-        if key not in my_bags.keys():
-            my_bags[key] = value
+    # get new bags
+    new_bags = get_contained_bags(prev_bags)
+    # add new bags to all bags
+    for bag, value in new_bags.items():
+        if bag not in my_bags.keys():
+            my_bags[bag] = value
         else:
-            my_bags[key] += value
-    # print(f"cycle: {cycle}")
-    cycle += 1
-    # print("My bags")
-    pp.pprint(my_bags)
-    # print("New bags added")
-    # pp.pprint(new_bags)
-
-# pp.pprint(my_bags)
+            my_bags[bag] += value
+    # move new bags into old for next cycle
+    prev_bags = new_bags.copy()
 
 sum = 0
-for value in my_bags.values():
-    # print(value)
-    sum += value
-# we dont count the shiny gold bag itself
-print(sum - 1)
+for bag, count in my_bags.items():
+    sum += count
+print(my_bags)
+print(sum)
