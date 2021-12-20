bd21979a19923660ddd8ab03700cdf24d22b2504
Eff <peter.felber@turbine.ai>
2021-12-20 16:24:08 +0100

    2021 dec 14/1

---
+++ 2021/dec14/1/main.py
@@ -0,0 +1,40 @@
+from copy import deepcopy
+from os import readlink
+url = '2021/dec14/input.txt'
+# url = '2021/dec14/input-example.txt'
+
+
+polimers = {}
+with open(url, 'r') as infile:
+    template = list(infile.readline().strip())
+    infile.readline()
+    rule = infile.readline().strip().split(' -> ')
+    while len(rule) > 1:
+        polimers[rule[0]]= rule[1]
+        rule = infile.readline().strip().split(' -> ')
+
+
+for i in range(10):
+    new_polimer = []
+    for idx, char in enumerate(template[1:]):
+        # print(f"{idx, char}")
+        new_polimer.append(template[idx])
+        pair = ''.join([template[idx], char])
+        if pair in polimers.keys():
+            new_polimer.append(polimers[pair])
+    new_polimer.append(char)
+    template = deepcopy(new_polimer)
+    # print(''.join(template))
+
+
+charset = set(template)
+print(charset)
+
+charcount = {}
+
+for char in charset:
+    print(f"{char}: {template.count(char)}")
+    charcount[char] = template.count(char)
+
+
+print(max(charcount.values()) - min(charcount.values()))
