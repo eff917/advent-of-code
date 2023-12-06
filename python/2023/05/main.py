3a1f1fd2253e467e6e0c944216d0350d91231c43
Peter Felber <peter.felber@turbine.ai>
2023-12-07 00:28:15 +0100

    2023 day05 part1

--- python/2023/05/main.py
+++ python/2023/05/main.py
@@ -5,25 +5,60 @@ import math
 part = "part1"
 infile = f"{os.path.dirname(os.path.realpath(__file__))}/{part}/input.txt"
 summary = 0
+map = {}
 with open(infile) as file:
     for line in file:
-        winning_numbers, my_numbers = line.split(":")[1].strip().split("|")
-        winning_numberstring = winning_numbers.strip().split(" ")
-        my_numberstring = my_numbers.strip().split(" ")
-        winning_numbers = set()
-        my_numbers = set()
-        for number in winning_numberstring:
-            if number.strip() != "":
-                winning_numbers.add(int(number.strip()))
-        for number in my_numberstring:
-            if number.strip() != "":
-                my_numbers.add(int(number.strip()))
-        power = len(my_numbers.intersection(winning_numbers)) - 1
-        if power >= 0:
-            summary += int(math.pow(2, power))
+        line = line.strip()
+        if line.startswith("seeds: "):
+            seeds = line.split(" ")[1:]
+        elif line == '':
+            continue
+        elif 'map' in line:
+            source, _, target = line.split(' ')[0].split('-')
+        else:
+            target_id, source_id,  range_value = line.split(' ')
+            offset = int(target_id) - int(source_id)
+            # TODO change this to lists inside map[destination]
+            if source not in map.keys():
+                map[source] = [{"target": target, "source_id": int(source_id), "offset": offset, "range_value": int(range_value)}]
+            else:
+                map[source].append({"target": target, "source_id": int(source_id), "offset": offset, "range_value": int(range_value)})
+for i in range(len(seeds)):
+    seeds[i] = int(seeds[i])
 
+target = "seed"
+next_target = map["seed"][0]["target"]
+id_map = {}
+for seed in seeds:
+    id_map[seed] = {"seed": seed}
+while next_target in map.keys():
+    for line in id_map.values():
+        seed = line[target]
+        range_found = False
+        for ranges in map[target]:
+            next_target = ranges["target"]
+            if seed in range(ranges["source_id"], ranges["source_id"]+ranges["range_value"]):
+                value = seed + ranges["offset"]
+                range_found = True
+        if not range_found:
+            value = seed
+        id_map[line["seed"]][next_target] = value
+        print(f"{target} value is {value} for seed {seed}")
+    target = next_target
+print(seeds)
+# print(id_map)
+minimum_location_id = None
+for key, value in id_map.items():
+    print(f"{key}: {value}")
+    if minimum_location_id is None:
+        minimum_location_id = value["location"]
+    if value["location"] < minimum_location_id:
+        minimum_location_id = value["location"]
+summary = minimum_location_id
 
 print(f"Part1 answer: {summary}")
+
+
 
 # endregion part1
 
@@ -32,33 +67,9 @@ part = "part2"
 infile = f"{os.path.dirname(os.path.realpath(__file__))}/{part}/input.txt"
 summary = 0
 
-cards = {}
 with open(infile) as file:
     for line in file:
-        card, numbers = line.strip().split(":")
-        card_number = int(card.strip().split(" ")[-1])
-        winning_numbers, my_numbers = numbers.strip().split("|")
-        winning_numberstring = winning_numbers.strip().split(" ")
-        my_numberstring = my_numbers.strip().split(" ")
-        winning_numbers = set()
-        my_numbers = set()
-        for number in winning_numberstring:
-            if number.strip() != "":
-                winning_numbers.add(int(number.strip()))
-        for number in my_numberstring:
-            if number.strip() != "":
-                my_numbers.add(int(number.strip()))
-        matches = len(my_numbers.intersection(winning_numbers))
-        # print(f"{card_number, matches}")
-        if card_number not in cards.keys():
-            cards[card_number] = 1
-        else:
-            cards[card_number] += 1
-        for i in range(1, matches+1):
-            if card_number+i not in cards.keys():
-                cards[card_number+i] = cards[card_number]
-            else:
-                cards[card_number+i] += cards[card_number]
-summary = sum(cards.values())
+        pass
+
 print(f"Part2 answer: {summary}")
 # endregion part2
