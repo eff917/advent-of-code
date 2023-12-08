966d62e212ddf91efca1fff018566bb6ad641856
Peter Felber <peter.felber@turbine.ai>
2023-12-08 23:46:18 +0100

    2023 day07 part1

--- python/2023/day07/part2/part2.py
+++ python/2023/day07/part2/part2.py
@@ -1,39 +1,97 @@
 import os
 import pprint
+from enum import IntEnum
 
 pp = pprint.PrettyPrinter(indent=2)
 
+
+def convert_hand(hand: str) -> str:
+    return (
+        hand.replace("T", "a")
+        .replace("J", "b")
+        .replace("Q", "c")
+        .replace("K", "d")
+        .replace("A", "e")
+    )
+
+
+# hand types: 5, 4, 3+2, 2+2, 2, 1
+def get_type(hand: str):
+    hand = sorted(hand)
+    type = 0
+    counts = [
+        hand.count(hand[0]),
+    ]
+    sum = counts[0]
+
+    while sum < 5:
+        next = hand.count(hand[sum])
+        counts.append(next)
+        sum += next
+
+    counts.sort(reverse=True)
+    print(counts)
+    if counts[0] > 3:
+        return counts[0] + 1
+    elif counts[0] == 3 and counts[1] == 2:
+        return 4
+    elif counts[0] == 3:
+        return 3
+    elif counts[0] == 2 and counts[1] == 2:
+        return 2
+    elif counts[0] == 1:
+        return 0
+    else:
+        return 1
+
+
+def compare_cards(card1: str, card2: str):
+    if Card_strength[card1] > Card_strength[card2]:
+        return 1
+    elif Card_strength[card1] < Card_strength[card2]:
+        return 2
+    else:
+        return 0
+
+
 def parse_input(path: str):
-    races = []
+    hands_list = {0: {}, 1: {}, 2: {}, 3: {}, 4: {}, 5: {}, 6: {}}
     with open(path) as file:
-        time = file.readline().strip().split(":")[1].strip().split()
-        distance = file.readline().strip().split(":")[1].strip().split()
+        for line in file:
+            hand, bid = line.split()
+            bid = int(bid)
+            hands_list[get_type(hand)][convert_hand(hand)] = bid
+    pp.pprint(hands_list)
+    return hands_list
 
-        time = int(''.join(time))
-        distance = int(''.join(distance))
 
-            
-    return (time, distance)
+Card_strength = IntEnum(
+    "Card_strength",
+    ["1", "2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K", "A"],
+)
+# TODO change cards to: 123456789abcde
+# this way simple comparison should work to determine the second ordering
+# T->a
+# J->b
+# Q->c
+# K->d
+# A->e
 
-def calculate_winning_options(time, distance):
-    # we spend i ms to get speed, so we travel (time-i)ms at i mm/ms
-    wins = 0
-    for i in range(time):
-        if (time-i)*i > distance:
-            wins += 1
-    return wins
 
 def main(infile):
-    time, distance = parse_input(infile)
-    winning_options = calculate_winning_options(time, distance)
+    hands_list = parse_input(infile)
+    answer = 0
+    rank = 1
+    for key, value in sorted(hands_list.items()):
+        for key2, value2 in sorted(value.items()):
+            print(f"Type: {key} Hand: {key2} Bid: {value2} Rank: {rank}")
+            answer += rank * value2
+            rank += 1
 
-    summary = winning_options
-    return summary
-
+    return answer
 
 
 if __name__ == "__main__":
     infile = f"{os.path.dirname(os.path.realpath(__file__))}/input.txt"
     result = main(infile)
     print(result)
-
--- python/2023/day07/part2/test_part2.py
+++ python/2023/day07/part2/test_part2.py
@@ -1,8 +1,38 @@
 import pytest
-from day07.part2 import part2
+from day07.part1 import part1
 import os
+
+@pytest.mark.parametrize(
+        "hand, expected",
+        [
+            ("12345", 0),
+            ("11234", 1),
+            ("11223", 2),
+            ("11122", 3),
+            ("11112", 4),
+            ("11111", 5)
+        ]
+)
+def test_hand_type(hand: str, expected: int):
+    actual = part1.get_type(hand=hand)
+    assert actual == expected
+
+@pytest.mark.parametrize(
+        "card1, card2, expected",
+        [
+            ("1", "1", 0),
+            ("1", "2", 2),
+            ("2", "1", 1),
+            ("1", "T", 2),
+            ("A", "T", 1)
+        ]
+)
+def test_card_strength(card1: str, card2: str, expected: int):
+    actual = part1.compare_cards(card1, card2)
+    assert actual == expected
 
 def test_given_input():
     infile = f"{os.path.dirname(os.path.realpath(__file__))}/test-input.txt"
-    actual_result = part2.main(infile)
-    assert actual_result == 71503
+    actual_result = part1.main(infile)
+    assert actual_result == 6440
+
