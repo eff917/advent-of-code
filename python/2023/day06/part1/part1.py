da3dc316d8cc40cf9203b97b4c6bc7f6be1ece6f
Peter Felber <peter.felber@turbine.ai>
2023-12-08 18:00:24 +0100

    pytest refactor

---
+++ python/2023/day06/part1/part1.py
@@ -0,0 +1,49 @@
+import os
+import pprint
+
+pp = pprint.PrettyPrinter(indent=2)
+
+def parse_input(path: str):
+    races = []
+    with open(path) as file:
+        times = file.readline().strip().split(":")[1].strip().split()
+        distances = file.readline().strip().split(":")[1].strip().split()
+        # for i in range(len(times)):
+        #     times.remove('')
+        # for _ in range(distances.count('')):
+        #     distances.remove('')
+        for i in range(len(times)):
+            races.append({"time": int(times[i]), "distance": int(distances[i])})
+            
+    return races
+
+def calculate_winning_options(time, distance):
+    # we spend i ms to get speed, so we travel (time-i)ms at i mm/ms
+    wins = 0
+    for i in range(time):
+        if (time-i)*i > distance:
+            wins += 1
+    return wins
+
+def main(infile):
+    race_list = parse_input(infile)
+    winning_options = []
+    for race in race_list:
+        winning_options.append(calculate_winning_options(race["time"], race["distance"]))
+
+
+    #TODO change this
+    # winning_options = [4,8,9]
+    summary = 1
+    for option in winning_options:
+        summary *= option
+
+    return summary
+
+
+
+if __name__ == "__main__":
+    infile = f"{os.path.dirname(os.path.realpath(__file__))}/input.txt"
+    result = main(infile)
+    print(result)
+
