29ae6918060296347cd88d5428e888cefd814818
Peter Felber <peter.felber@turbine.ai>
2023-12-09 09:05:43 +0100

    2023 08 p1

--- python/2023/day08/part1/test_part1.py
+++ python/2023/day08/part1/test_part1.py
@@ -2,38 +2,16 @@ import pytest
 from day08.part1 import part1
 import os
 
-@pytest.mark.parametrize(
-        "hand, expected",
-        [
-            ("12345", 0),
-            ("11234", 1),
-            ("11223", 2),
-            ("11123", 3),
-            ("11122", 4),
-            ("11112", 5),
-            ("11111", 6)
-        ]
-)
-def test_hand_type(hand: str, expected: int):
-    actual = part1.get_type(hand=hand)
-    assert actual == expected
 
 @pytest.mark.parametrize(
-        "card1, card2, expected",
+        "inpufilename, expected",
         [
-            ("1", "1", 0),
-            ("1", "2", 2),
-            ("2", "1", 1),
-            ("1", "T", 2),
-            ("A", "T", 1)
+            ("test-input.txt", 2),
+            ("test-input-2.txt", 6),
         ]
 )
-def test_card_strength(card1: str, card2: str, expected: int):
-    actual = part1.compare_cards(card1, card2)
-    assert actual == expected
-
-def test_given_input():
-    infile = f"{os.path.dirname(os.path.realpath(__file__))}/test-input.txt"
+def test_given_input(inpufilename, expected):
+    infile = f"{os.path.dirname(os.path.realpath(__file__))}/{inpufilename}"
     actual_result = part1.main(infile)
-    assert actual_result == 6440
+    assert actual_result == expected
 
