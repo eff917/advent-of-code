777d585316cbad91e33332f8feddb8aa412c829c
Peter Felber <peter.felber@turbine.ai>
2023-12-10 08:33:44 +0100

    2023 day10 init

---
+++ python/2023/day10/part1/test_part1.py
@@ -0,0 +1,46 @@
+import pytest
+from day09.part1 import part1
+import os
+
+
+@pytest.mark.parametrize(
+    "inpufilename, expected",
+    [
+        ("test-input.txt", 114),
+    ],
+)
+def test_given_input(inpufilename, expected):
+    infile = f"{os.path.dirname(os.path.realpath(__file__))}/{inpufilename}"
+    actual_result = part1.main(infile)
+    assert actual_result == expected
+
+
+@pytest.mark.parametrize(
+    "bottom_line, top_line, expected",
+    [([0, 0, 0, 0, 0], [3, 3, 3, 3, 3], [3, 3, 3, 3, 3, 3])],
+)
+def test_extend_top_line(bottom_line, top_line, expected):
+    actual = part1.extend_top_line(bottom_line, top_line)
+    assert actual == expected
+
+
+@pytest.mark.parametrize(
+    "submap, expected",
+    [
+        ([[0, 3, 6, 9, 12, 15], [3, 3, 3, 3, 3], [0, 0, 0, 0]], 18),
+        ([[1, 3, 6, 10, 15, 21], [2, 3, 4, 5, 6], [1, 1, 1, 1], [0, 0, 0]], 28),
+        (
+            [
+                [10, 13, 16, 21, 30, 45],
+                [3, 3, 5, 9, 15],
+                [0, 2, 4, 6],
+                [2, 2, 2],
+                [0, 0],
+            ],
+            68,
+        ),
+    ],
+)
+def test_get_submap_extension_value(submap, expected):
+    actual = part1.get_submap_extension_value(submap)
+    assert actual == expected
---
+++ python/2023/day10/part2/part2.py
@@ -0,0 +1,55 @@
+import os
+import pprint
+from enum import IntEnum
+
+pp = pprint.PrettyPrinter(indent=2)
+
+def calc_diff(line):
+    history = []
+    elem1 = line[0]
+    for elem2 in line[1:]:
+        history.append(elem2-elem1)
+        elem1 = elem2
+
+    return history
+
+def extend_top_line(bottom_line, top_line):
+    top_line.append(top_line[-1]+bottom_line[-1])
+    return top_line
+
+
+def get_submap_extension_value(submap: list):
+    submap.reverse()
+    submap[0].append(0)
+    for i in range(1, len(submap)):
+        submap[i] = extend_top_line(submap[i-1], submap[i])
+    return submap[-1][-1]
+
+def parse_input(path: str):
+    map = []
+    with open(path) as file:
+        for line in file:
+            line = [int(x) for x in line.split()]
+            line.reverse()
+            submap = [line,]
+            # print(line)
+            while max(line) > 0 or min(line) < 0:
+                line = calc_diff(line)
+                submap.append(line)
+                # print(line)
+            map.append(submap)
+
+    return map
+
+def main(infile):
+    answer = 0
+    map = parse_input(infile)
+    pp.pprint(map)
+    for submap in map:
+        answer += get_submap_extension_value(submap)
+    return answer
+
+if __name__ == "__main__":
+    infile = f"{os.path.dirname(os.path.realpath(__file__))}/input.txt"
+    answer = main(infile)
+    print(answer)
