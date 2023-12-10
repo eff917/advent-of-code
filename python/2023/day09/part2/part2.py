27fa5a0e3bd11fd7b70fbff010ea190bc0d1f044
Peter Felber <peter.felber@turbine.ai>
2023-12-10 08:33:14 +0100

    2023 day09 part2

--- python/2023/day09/part2/part2.py
+++ python/2023/day09/part2/part2.py
@@ -1,86 +1,55 @@
 import os
 import pprint
-import math
+from enum import IntEnum
 
 pp = pprint.PrettyPrinter(indent=2)
 
+def calc_diff(line):
+    history = []
+    elem1 = line[0]
+    for elem2 in line[1:]:
+        history.append(elem2-elem1)
+        elem1 = elem2
+
+    return history
+
+def extend_top_line(bottom_line, top_line):
+    top_line.append(top_line[-1]+bottom_line[-1])
+    return top_line
+
+
+def get_submap_extension_value(submap: list):
+    submap.reverse()
+    submap[0].append(0)
+    for i in range(1, len(submap)):
+        submap[i] = extend_top_line(submap[i-1], submap[i])
+    return submap[-1][-1]
+
 def parse_input(path: str):
-    map = {}
+    map = []
     with open(path) as file:
         for line in file:
-            line = line.split()
-            if len(line)==1:
-                instructions = line[0]
-            elif len(line) == 4:
-                node = line[0]
-                left = line[2][1:4]
-                right = line[3][0:3]
-                map[node] = {"L": left, "R": right}
-    return (instructions, map)
+            line = [int(x) for x in line.split()]
+            line.reverse()
+            submap = [line,]
+            # print(line)
+            while max(line) > 0 or min(line) < 0:
+                line = calc_diff(line)
+                submap.append(line)
+                # print(line)
+            map.append(submap)
 
-def step(position, instruction, map):
-    return map[instruction]
-
-def arrived(positions):
-    for position in positions:
-        if position[-1] != "Z":
-            return False
-    return True
+    return map
 
 def main(infile):
-    instructions, map = parse_input(infile)
-    positions = list(filter(lambda x: (x[-1] == "A"), map.keys()))  
-    print(positions)
-    instruction_length = len(instructions)
-    # pp.pprint(f"Map:\n{map}")
-    # print(instructions)
-    steps_to_arrive = []
-    steps_to_loop = []
-    loop_stepcount = []
-    print(f"Positions: {positions}")
     answer = 0
-    for sposition in positions:
-        position = sposition
-        arrived = 0
-        i = 0
-        while position[-1] != 'Z' or arrived < 2:
-            position = step(position=position, instruction=instructions[i%instruction_length], map=map[position])
-            print(f"{sposition} {position} {i}")
-            i += 1
-            if position[-1] == 'Z':
-                if arrived == 0:
-                    steps_to_arrive.append(i)
-                    print(f"{arrived}: {i}")
-                    arrived +=1
-                elif arrived == 1:
-                    steps_to_loop.append(i)
-                    arrived += 1
-        print(f"{i} steps required for {position}")
-    answer = i
-
-    p_count = len(positions)
-    for i in range(p_count):
-        loop_stepcount.append(steps_to_loop[i]-steps_to_arrive[i])
-
-    x = steps_to_arrive[0]
-    step_value = loop_stepcount[0]
-    found = False
-
-    print(f"Inital steps required: {steps_to_arrive}")
-    print(f"Steps in loop        : {loop_stepcount}")
-
-    lcm = math.lcm(loop_stepcount[0], loop_stepcount[1])
-    for i in range(2,p_count):
-        lcm = math.lcm(lcm, loop_stepcount[i])
-    print(f"Least common multiple = {lcm}")
-
-    answer = lcm
-    print(f"Answer: {answer}")
+    map = parse_input(infile)
+    pp.pprint(map)
+    for submap in map:
+        answer += get_submap_extension_value(submap)
     return answer
-
 
 if __name__ == "__main__":
     infile = f"{os.path.dirname(os.path.realpath(__file__))}/input.txt"
     answer = main(infile)
     print(answer)
-
--- python/2023/day09/part2/test_part2.py
+++ python/2023/day09/part2/test_part2.py
@@ -2,13 +2,45 @@ import pytest
 from day09.part2 import part2
 import os
 
+
 @pytest.mark.parametrize(
     "inpufilename, expected",
     [
-        ("test-input-3.txt", 6),
+        ("test-input.txt", 2),
     ],
 )
 def test_given_input(inpufilename, expected):
     infile = f"{os.path.dirname(os.path.realpath(__file__))}/{inpufilename}"
     actual_result = part2.main(infile)
     assert actual_result == expected
+
+
+@pytest.mark.parametrize(
+    "bottom_line, top_line, expected",
+    [([0, 0, 0, 0, 0], [3, 3, 3, 3, 3], [3, 3, 3, 3, 3, 3])],
+)
+def test_extend_top_line(bottom_line, top_line, expected):
+    actual = part2.extend_top_line(bottom_line, top_line)
+    assert actual == expected
+
+
+@pytest.mark.parametrize(
+    "submap, expected",
+    [
+        ([[15, 12, 9, 6, 3, 0], [-3, -3, -3, -3, -3], [0, 0, 0, 0]], -3),
+        ([[21, 15, 10, 6, 3, 1], [-6, -5, -4, -3, -2], [1, 1, 1, 1], [0, 0, 0]], 0),
+        (
+            [
+                [45, 30, 21, 16, 13, 10],
+                [-15, -9, -5, -3, -3],
+                [6, 4, 2, 0],
+                [-2, -2, -2],
+                [0, 0]
+            ],
+            5,
+        ),
+    ],
+)
+def test_get_submap_extension_value(submap, expected):
+    actual = part2.get_submap_extension_value(submap)
+    assert actual == expected
