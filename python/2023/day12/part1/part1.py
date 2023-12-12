441358fbdc070ca3324378b576177024a37cc176
Peter Felber <peter.felber@turbine.ai>
2023-12-12 16:31:00 +0100

    2023 day12 part1

--- python/2023/day12/part1/part1.py
+++ python/2023/day12/part1/part1.py
@@ -4,78 +4,54 @@ import pprint
 pp = pprint.PrettyPrinter(indent=2)
 
 
-def expand_lines(map: list[str]) -> list[str]:
-    new_map = []
-    for line in map:
-        new_map.append(line)
-        if line.count(".") == len(line):
-            new_map.append(line)
-    return new_map
-
-
-def expand_columns(map: list[str]) -> list[str]:
-    i = 0
-    column = ""
-    while i < len(map[0]):
-        empty = True
-        for line in map:
-            if line[i] != ".":
-                empty = False
-        if empty:
-            for y in range(len(map)):
-                map[y] = map[y][: i + 1] + map[y][i:]
-            i += 1
-        i += 1
-    return map
-
-
-def expand_map(map: list[str]) -> list[str]:
-    map = expand_lines(map=map)
-    map = expand_columns(map=map)
-    return map
-
-
-def replace_galaxies(map: list[str]) -> list[str]:
-    galaxy_list = {}
-    index = 1
-    for y, line in enumerate(map):
-        for x, char in enumerate(line):
-            if char == "#":
-                galaxy_list[index] = (y, x)
-                index += 1
-    return galaxy_list
-
-
-def get_distance(galaxy_list: list, galaxy_1: int, galaxy_2: int) -> int:
-    return abs(galaxy_list[galaxy_1][0]-galaxy_list[galaxy_2][0]) + abs(galaxy_list[galaxy_1][1]-galaxy_list[galaxy_2][1])
-
-
 def parse_input(filename: str):
     path = f"{os.path.dirname(os.path.realpath(__file__))}/{filename}"
     map = []
+    damage_groups = []
     with open(path) as file:
         for line in file:
-            line = line.strip()
-            map.append(line)
-    return map
+            springs, damages = line.split()
+            damages = damages.split(',')
+            damages = list(int(damage) for damage in damages)
+            map.append(springs)
+            damage_groups.append(damages)
+    return (map, damage_groups)
+
+def get_damages(line):
+    line = line.split(".")
+    i=0
+    while i< len(line):
+        if "#" not in line[i]:
+            del line[i]
+        else:
+            i+=1
+    damages = []
+    for damage in line:
+        damages.append(len(damage))
+    return damages
+    
+def get_line_permutations(line, damages):
+    permutations = 0
+    index = line.find("?")
+    if index == -1:
+        if get_damages(line) == damages:
+            return 1
+        else:
+            return 0
+    else:
+        line1 = f"{line[:index]}.{line[index+1:]}"
+        permutations += get_line_permutations(line1, damages)
+        line2 = f"{line[:index]}#{line[index+1:]}"
+        permutations += get_line_permutations(line2, damages)
+        return permutations
+
 
 
 def main(infile):
     answer = 0
-    map = parse_input(infile)
-    print("Map parsed")
-    map = expand_map(map)
-    print("Map expanded")
-    galaxy_list = replace_galaxies(map)
-    print("Galaxies replaced")
-
-    # pp.pprint(map)
-    # TODO: refactor replace galaxies, to return a list of galaxies with coordinates
-    # that way we don't need to loop over the map for every connection
-    for key in galaxy_list.keys():
-        for i in range(1,key):
-            answer += get_distance(galaxy_list, key, i)
-
+    map, damage_groups = parse_input(infile)
+    for i in range(len(map)):
+        answer += get_line_permutations(map[i], damage_groups[i])
     return answer
 
 
--- python/2023/day12/part1/test_part1.py
+++ python/2023/day12/part1/test_part1.py
@@ -8,7 +8,7 @@ import os
 @pytest.mark.parametrize(
     "inpufilename, expected",
     [
-        ("test-input.txt", 374),
+        ("test-input.txt", 21),
     ],
 )
 def test_main_answer(inpufilename, expected):
@@ -19,51 +19,33 @@ def test_main_answer(inpufilename, expected):
 
 @pytest.mark.day12
 @pytest.mark.parametrize(
-    "infile, expected_file", [("test-input.txt", "test-input-expanded.txt")]
+    "input, expected",
+    [
+        ("#.#.###", [1, 1, 3]),
+        (".#...#....###.", [1, 1, 3]),
+        (".#.###.#.######", [1, 3, 1, 6]),
+        ("####.#...#...", [4, 1, 1]),
+        ("#....######..#####.", [1, 6, 5]),
+        (".###.##....#", [3, 2, 1]),
+    ],
 )
-def test_expand(infile, expected_file):
-    map = part.parse_input(infile)
-    actual_map = part.expand_map(map=map)
-    expected_map = part.parse_input(expected_file)
-
-    assert actual_map == expected_map
+def test_get_damages(input, expected):
+    actual = part.get_damages(input)
+    assert actual == expected
 
 
 @pytest.mark.day12
 @pytest.mark.parametrize(
-    "infile, expected_list",
+    "line, damages, expected",
     [
-        ("test-input-expanded.txt", {
-        1: (0, 4),
-        2: (1, 9),
-        3: (2, 0),
-        4: (5, 8),
-        5: (6, 1),
-        6: (7, 12),
-        7: (10, 9),
-        8: (11, 0),
-        9: (11, 5),
-    }),
+        ("???.###", [1, 1, 3], 1),
+        (".??..??...?##.", [1, 1, 3], 4),
+        ("?#?#?#?#?#?#?#?", [1, 3, 1, 6], 1),
+        ("????.#...#...", [4, 1, 1], 1),
+        ("????.######..#####.", [1, 6, 5], 4),
+        ("?###????????", [3, 2, 1], 10),
     ],
 )
-def test_galaxy_list(infile, expected_list):
-    in_map = part.parse_input(infile)
-    actual_list = part.replace_galaxies(in_map)
-    assert actual_list == expected_list
-
-
-@pytest.mark.day12
-@pytest.mark.parametrize(
-    "infile, galaxy_1, galaxy_2, expected",
-    [
-        ("test-input-expanded.txt", 5, 9, 9),
-        ("test-input-expanded.txt", 1, 7, 15),
-        ("test-input-expanded.txt", 3, 6, 17),
-        ("test-input-expanded.txt", 8, 9, 5),
-    ],
-)
-def test_distance(infile, galaxy_1, galaxy_2, expected):
-    map = part.parse_input(infile)
-    list = part.replace_galaxies(map)
-    actual = part.get_distance(list, galaxy_1, galaxy_2)
+def test_line_permutations(line, damages, expected):
+    actual = part.get_line_permutations(line, damages)
     assert actual == expected
